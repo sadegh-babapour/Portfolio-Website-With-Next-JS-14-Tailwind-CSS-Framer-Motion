10e6bb939a008c643ed2c6572f94f0d6f60f401a
chakorn.san <chakorn.san@cpf.co.th>
2024-01-10 17:44:44 +0700

    Add portfolio

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import Socials from "./Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:pl-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between xl:flex-row gap-y-6 py-8">
+          <Link href={"/"}>
+            <Image
+              src="/logo.svg"
+              width={220}
+              height={48}
+              alt="logo"
+              priority={true}
+            />
+          </Link>
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="relative inline-flex  group">
+      <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+      </div>
+      <Link href={'/work'}
+        className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-full focus:none focus:ring-gray-900"
+      >My Projects
+      </Link>
+    </div>
+  )
 };
 
 export default ProjectsBtn;
